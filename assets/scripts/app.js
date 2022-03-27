23478f79a079c1ae7e8fe9def52594cf07c2ca2e
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-03-27 09:35:36 +0600

    Promisifying the HTTP request code using the async / await method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -20,16 +20,15 @@ function sendHttpRequest(method, url) {
   return promise;
 }
 
-function fetchPosts() {
-  sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts").then((responseData) => {
-    const listOfPosts = responseData;
-    for (const post of listOfPosts) {
-      const postEl = document.importNode(postTemplate.content, true);
-      postEl.querySelector("h2").textContent = post.title.toUpperCase();
-      postEl.querySelector("p").textContent = post.body;
-      listElement.append(postEl);
-    }
-  });
+async function fetchPosts() {
+  const responseData = await sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts");
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
 }
 
 fetchPosts();
