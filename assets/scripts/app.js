e2091c479e3111e861a0b9397b5641c9efd374f6
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-03-27 11:16:58 +0600

    Converting XMLHttpRequest to fetch() in order to handle GET requests

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,46 +5,44 @@ const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
+  //   const promise = new Promise((resolve, reject) => {
+  // const xhr = new XMLHttpRequest();
+  // xhr.open(method, url);
+  // xhr.responseType = "json";
+  // xhr.onload = function () {
+  //   if (xhr.status >= 200 && xhr.status < 300) {
+  //     resolve(xhr.response);
+  //   } else {
+  //     reject(new Error("Something went wrong!"));
+  //   }
+  //   //   const lisOfPosts = JSON.parse(xhr.response);
+  // };
+  // xhr.onerror = function () {
+  //   reject(new Error("Failed to send request!"));
+  // };
+  // xhr.send(JSON.stringify(data));
+  //   });
 
-    xhr.open(method, url);
-
-    xhr.responseType = "json";
-
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject(new Error("Something went wrong!"));
-      }
-      //   const lisOfPosts = JSON.parse(xhr.response);
-    };
-
-    xhr.onerror = function () {
-      reject(new Error("Failed to send request!"));
-    };
-
-    xhr.send(JSON.stringify(data));
+  //   return promise;
+  return fetch(url).then((response) => {
+    return response.json();
   });
-
-  return promise;
 }
 
 async function fetchPosts() {
-  try {
-    const responseData = await sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/pos");
-    const listOfPosts = responseData;
-    for (const post of listOfPosts) {
-      const postEl = document.importNode(postTemplate.content, true);
-      postEl.querySelector("h2").textContent = post.title.toUpperCase();
-      postEl.querySelector("p").textContent = post.body;
-      postEl.querySelector("li").id = post.id;
-      listElement.append(postEl);
-    }
-  } catch (error) {
-    alert(error.message);
+  //   try {
+  const responseData = await sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts");
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
+    listElement.append(postEl);
   }
+  //   } catch (error) {
+  //     alert(error.message);
+  //   }
 }
 
 async function createPost(title, content) {
@@ -73,5 +71,3 @@ postList.addEventListener("click", (event) => {
     sendHttpRequest("DELETE", `https://jsonplaceholder.typicode.com/posts/${postId}`);
   }
 });
-
-//In line 36 you removed "ts" from the "posts" word in the URL, so that you could test the error handling code you wrote
